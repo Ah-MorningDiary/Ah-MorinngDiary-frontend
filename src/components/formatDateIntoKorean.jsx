bad3fa1a71bc41bae43b93fc91642d32af7599e1
DingX2 <pourding@naver.com>
2023-11-12 00:23:38 +0900

    [TEST] 머지 진행중

--- src/components/formatDateIntoKorean.jsx
+++ src/components/formatDateIntoKorean.jsx
@@ -1,8 +1,5 @@
 export const formatDateIntoKorean = (writeDate) => {
   //{2023-11-11} -> 2023년 11월 11일로 만드는 함수
-  console.log(writeDate);
-export const formatDateIntoKorean = (writeDate) => {
-  // 입력 문자열을 Date 객체로 파싱
   const dateObject = new Date(writeDate);
   const options = { year: "numeric", month: "long", day: "numeric" };
   const formattedDate = dateObject.toLocaleDateString("ko-KR", options);
