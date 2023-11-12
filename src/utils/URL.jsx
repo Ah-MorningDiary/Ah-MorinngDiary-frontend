6ff3774c593cc0bf70921ccda6a2b1c9f722391c
DingX2 <pourding@naver.com>
2023-11-12 23:13:08 +0900

    [DEPLOY] v4 배포

--- src/utils/URL.jsx
+++ src/utils/URL.jsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://3.39.139.234:8080";
+export const BASE_URL = "https://amoringdiary.com";
+//export const BASE_URL = "http://3.39.139.234:8080";
 export const BASE_URL_FRONT = "http://프론트홈서버";
 export const BASE_STT_URL = "https://speech.googleapis.com";
 export const IMAGE_API = "/api/v1/image/upload";
