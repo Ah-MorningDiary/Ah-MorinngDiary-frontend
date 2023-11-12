40665370abb3da12623e7e5561dbf8c6861dfbcd
joojjang <itchu999@naver.com>
2023-11-12 12:13:26 +0900

    [REFACTOR] Chart 페이지 컴포넌트화

--- src/pages/Chart/Chart.jsx
+++ src/pages/Chart/Chart.jsx
@@ -7,37 +7,52 @@ import axios from "axios";
 import { BASE_URL } from "../../utils/URL";
 
 function Chart() {
+  const Title = ( { title, content } ) => {
+    return (
+      <section className='chart-title'>
+        <h1>{title}</h1>
+        <p>{content}</p>
+      </section>
+    );
+  }
+
+  const ContainerDoromari = () => {
+    return (
+      <section className="chart-container container-doromari">
+        <div className="item"></div>
+      </section>
+    );
+  }
+
+  const ContainerBookBlank = () => {
+    return (
+      <section>
+        <img
+          src={bookBlank_edge}
+          alt="bookBlank_edge"
+          style={{ display: "inline-block", verticalAlign: "bottom", width: "100%", maxWidth: "1280px", }}
+        />
+        <div className="chart-container container-bookBlank">
+          {/* 차트 내용 */}
+        </div>
+        <img
+          src={bookBlank_edge}
+          alt="bookBlank_edge"
+          style={{ transform: "scaleY(-1)", width: "100%", maxWidth: "1280px", }}
+        />
+      </section>
+    );
+  }
+
   return (
     <>
       <HomeButton />
 
       <main className="chart-page">
         <section className="chart-wrapper">
-          <section className='chart-title'>
-            <h1>나의 기억 건강 관리</h1>
-            <p>월별 건강 상태의 추이를 살피고 관리해보세요.</p>
-          </section>
+          <Title title="나의 기억 건강 관리" content="월별 건강 상태의 추이를 살피고 관리해보세요." />
 
-          <section>
-            <img
-              src={bookBlank_edge}
-              alt="bookBlank_edge"
-              style={{ display: "inline-block", verticalAlign: "bottom", width: "100%", maxWidth: "1280px", }}
-            />
-            <div className="chart-container container-bookBlank">
-              
-            </div>
-            <img
-              src={bookBlank_edge}
-              alt="bookBlank_edge"
-              style={{ transform: "scaleY(-1)", width: "100%", maxWidth: "1280px", }}
-            />
-          </section>
-
-          {/* <section className="chart-container container-doromari">
-            <div className="item"></div>
-          </section> */}
-
+          <ContainerBookBlank />
         </section>
       </main>
     </>
