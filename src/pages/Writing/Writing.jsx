44d9b2f06f061a12cb8f8ba2354146f7960db6b5
DingX2 <pourding@naver.com>
2023-11-12 21:15:44 +0900

    [FEAT] 옛 파일 확인, 저장후 올리기

--- src/pages/Writing/Writing.jsx
+++ src/pages/Writing/Writing.jsx
@@ -43,7 +43,7 @@ export default function Writing() {
     setImage(imageUrl);
     setData({
       ...data,
-      imgUrl: imageUrl,
+      imgUrl: image,
     });
     console.log(image, "image");
   };
